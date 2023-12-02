4f50d0496549570be57f829eca81a7dbe955e661
osaydayoub <osaydayoub@gmail.com>
2023-12-02 14:13:47 +0200

    footer css  added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
+import './Footer.css'
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-
+import { RxDividerVertical } from "react-icons/rx";
+import { AiOutlineCopyright } from "react-icons/ai";
 function Footer() {
-  return (
-    <div className='footer-container'>
-        <h3>Copyright Osayd Ayoub 2023</h3>
-        <a href="https://www.linkedin.com/"><FaLinkedin/></a>
-        <a href="https://github.com/osaydayoub"><FaGithub /></a>
-        <a href="https://twitter.com/home"><FaXTwitter /></a>
+    return (
+        <div className='footer-container'>
+            <p><AiOutlineCopyright /> Copyright Osayd Ayoub  2023</p>
 
-
-
-        
-    </div>
-  )
+            <div className='icon-list'>
+                <a href="https://www.linkedin.com/"><FaLinkedin /></a>
+                <RxDividerVertical />
+                <a href="https://github.com/osaydayoub"><FaGithub /></a>
+                <RxDividerVertical />
+                <a href="https://twitter.com/home"><FaXTwitter /></a>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
